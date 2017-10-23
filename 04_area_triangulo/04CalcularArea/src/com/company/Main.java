8417afdb99c48e6b60daa8e4aa89977ba457902b
Unknown <Insanelik31@gmail.com>
2017-10-23 20:28:15 +0200

    Ejercicio 4

---
+++ 04_area_triangulo/04CalcularArea/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            double b, h;
+            System.out.println("Ingresa base");
+            b = sc.nextDouble();
+            System.out.println("Ingresa altura");
+            h = sc.nextDouble();
+            double area;
+            area = b * h / 2;
+            System.out.print(area);
+        }
+    }
