197deb9f6b257772816d0096aa2d4018eb95cb06
Unknown <Insanelik31@gmail.com>
2017-10-23 20:42:50 +0200

    Ejercicio 18

---
+++ 18_es_primo/18NumeroPrimo/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    //Me gustaria que repitiera hasta decir fin
+    public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int a=0,i,n;
+                System.out.println("Ingrese numero");
+                n=sc.nextInt();
+                for(i=1;i<(n+1);i++){
+                    if(n%i==0){
+                        a++;
+                    }
+                }
+                if(a!=2){
+                    System.out.println("No es Primo");
+                }else{
+                    System.out.println("Si es Primo");
+                }
+            }
+        }
