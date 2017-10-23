d0c39b996ea0717c659dbc34cf92f904525af8bb
Unknown <Insanelik31@gmail.com>
2017-10-23 20:34:31 +0200

    Ejercicio 6

---
+++ 06_par_impar/06ParOImpar/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa un numero ");
+        int a = sc.nextInt();
+        if (a % 2 == 0) {
+            System.out.println(a + " es par");
+        }
+        else {
+            System.out.println(a + " es impar");
+        }
+    }
+}
